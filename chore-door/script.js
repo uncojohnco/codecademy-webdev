163ce0c65a31404a934f85dbea1fa9e5839072d6
johnco <dev@johnco.org>
2020-06-11 11:10:39 -0400

    chore-door - latest

--- chore-door/script.js
+++ chore-door/script.js
@@ -1,50 +1,119 @@
 const doorPaths = {
-    closed: 'https//s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg',
-    robot: 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg',
-    beach: 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg',
-    space: 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg',
+  closed: 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg',
+  robot: 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg',
+  beach: 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg',
+  space: 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg',
 };
 
-const doorFactory = (doorid) => {
-    return {
-        _doorid: doorid,
-        _srcEle: document.getElementById(doorid),
-        _open: false,
 
-        init() {
-            this._srcEle.onclick = this.onclick;
-        },
+const openDoors = [
+  {
+    name: 'beach',
+    img: doorPaths.beach
+  },
+  {
+    name: 'space',
+    img: doorPaths.space
+  },
+  {
+    name: 'robot',
+    img: doorPaths.robot
+  },
+]
 
-        onclick() {
-            if (this._open) {
-                this._srcEle.src = doorPaths.closed;
-                this._open = false;
-            } else {
-                this._srcEle.src = doorAttrs[this._doorid].img;
-                this._open = true;
-            };
-        },
-    }
-};
 
-const doorAttrs = {
-    door1: { img: doorPaths.robot },
-    door2: { img: doorPaths.beach },
-    door3: { img: doorPaths.space }
+function getRandomOpenDoor() {
+  const index = Math.floor(Math.random() * openDoors.length);
+  return openDoors[index];
 }
 
 
-doorOnclick = (event) => {
-    const door = event.srcElement;
-    console.log(door);
-    door.src = doorAttrs[door.id].img;
+const doorOnclick = (door) => {
+
+  // console.log(e.target.id);
+
+  if (door._open) {
+    door.src = doorPaths.closed;
+    door._open = false;
+    door._behind = null;
+
+  } else {
+    const openDoor = getRandomOpenDoor()
+
+    door.src = openDoor.img;
+    door._open = true;
+    door._behind = openDoor.name;
+  }
+
+  return door;
+
 };
 
 
-const doorObjs = {
-    door1: doorFactory('door1'),
-    door2: doorFactory('door2'),
-    door3: doorFactory('door3'),
+const createDoor = (doorid) => {
+
+  const element = document.getElementById(doorid);
+  element._open = false;
+  element._behind = null;
+  element.onclick = game.playDoor;
+
+  return element;
 };
 
-doorObjs.door1.init()
+
+const game = {
+
+  _doors: {},
+
+  addDoor(name) {
+    this._doors[name] = createDoor(name)
+  },
+
+  get doors() {
+    return Object.entries(this._doors)
+  },
+
+  getRound() {
+    game._doors.forEach(console.log(d => this._doors[d]));
+  },
+
+  isRoundOver() {
+
+    let behind;
+
+    for ([k, d] of this.doors) {
+      if (behind === undefined) {
+        behind = d._behind;
+      } else {
+        if (behind !== d._behind) {
+          return false
+        }
+      }
+    }
+
+    return true;
+  },
+
+
+  playDoor (e) {
+    const door = e.target.id;
+
+    doorOnclick(door);
+    // 'this' returns the element that fired this event!
+    // Should work out a better way to handle this when in an inner js objects...
+    // this.doors
+    game.doors.forEach(([k, d]) => console.log(k, d._behind));
+
+    if (this.isRoundOver()) {
+      this.endRound();
+    }
+
+  },
+
+
+};
+
+
+game.addDoor('door1');
+game.addDoor('door2');
+game.addDoor('door3');

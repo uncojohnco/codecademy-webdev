3bf8b2fa4406a982a5b4a4325f6d1bcee850ce70
johnco <dev@johnco.org>
2020-06-17 10:55:38 -0400

    implement `fetch` variant for handling GET request - lessons/xhr-get-requests-datamuse

---
+++ lessons/xhr-get-requests-datamuse/main-fetch.js
@@ -0,0 +1,48 @@
+// Information to reach API
+const url = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_rhy=';
+
+
+// Selecting page elements
+const inputField = document.querySelector('#input');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
+
+
+// AJAX function
+const getSuggestions = () => {
+
+  const wordQuery = inputField.value;
+  const endpoint = url + queryParams + wordQuery;
+
+  fetch(endpoint, { cache: "no-cache" })
+    .then(
+      (response) => {
+        if (response.ok) {
+          console.log(response);
+          return response.json();
+        }
+        throw new Error("Request failed!");
+      },
+      (networkError) => {
+        console.log(networkError.message);
+      }
+    )
+    .then((jsonResponse) => {
+      renderResponse(jsonResponse, responseField);
+    });
+
+}
+
+// Clear previous results and display results to webpage
+const displaySuggestions = (event) => {
+  event.preventDefault();
+
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  };
+  
+  getSuggestions();
+}
+
+submit.addEventListener('click', displaySuggestions);
